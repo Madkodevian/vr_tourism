4de3462caa598f75d9c832569ce57eafd83a48b3
Tatiana Rosales Hernández <tatianarh97@gmail.com>
2021-04-25 14:01:59 +0100

    commitnuevo

--- js/form.js
+++ js/form.js
@@ -181,3 +181,26 @@ function logout() {
     console.log(error.message)
   });
 }
+
+//PAGINA INICIO SESION
+function show() {
+  var p = document.getElementById('pwd');
+  p.setAttribute('type', 'text');
+}
+
+function hide() {
+  var p = document.getElementById('pwd');
+  p.setAttribute('type', 'password');
+}
+
+var pwShown = 0;
+
+document.getElementById("eye").addEventListener("click", function () {
+  if (pwShown == 0) {
+      pwShown = 1;
+      show();
+  } else {
+      pwShown = 0;
+      hide();
+  }
+}, false);
